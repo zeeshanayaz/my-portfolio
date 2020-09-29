9d4d44c57fbae2850b90773e25de178d41f4fa64
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2020-09-29 20:52:20 +0500

    build push

---
+++ precache-manifest.4a121b09c317c659bae9653e0b4665d9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dad162563e65ea0ae4feb8140690c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463c48de09bfb424179d",
+    "url": "/static/css/3.70d62d19.chunk.css"
+  },
+  {
+    "revision": "19465b9d1b89caa1533e",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "0deaec3de3a632c5bb6d",
+    "url": "/static/css/main.74351ba7.chunk.css"
+  },
+  {
+    "revision": "0bd960625d10681c03d8",
+    "url": "/static/js/2.43be07b9.chunk.js"
+  },
+  {
+    "revision": "463c48de09bfb424179d",
+    "url": "/static/js/3.805393a6.chunk.js"
+  },
+  {
+    "revision": "19465b9d1b89caa1533e",
+    "url": "/static/js/4.6ae9a176.chunk.js"
+  },
+  {
+    "revision": "0deaec3de3a632c5bb6d",
+    "url": "/static/js/main.906028db.chunk.js"
+  },
+  {
+    "revision": "d415dcbd48e1d418f505",
+    "url": "/static/js/runtime-main.5e923a45.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5a8b6814b84004bd2a4ad5f6a37c9e56",
+    "url": "/static/media/karachiUniversity.5a8b6814.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3113eb6220d2c495567910fe6ef24f3d",
+    "url": "/static/media/startupAMDS.3113eb62.png"
+  }
+]);
